aefff0e1d5211da8d3da22fb4cf9e7f9dd1db7d7
Julia Fong <Juliamzfong@gmail.com>
2020-09-16 13:55:41 -0500

    Update notes on json

--- 1-json.js
+++ 1-json.js
@@ -1,9 +1,37 @@
+const fs = require('fs')
+
+
 // data stored as js object
 // convert this into json
-const book = {
-    title: 'Human Acts',
-    author: 'Han Kang'
-}
+// const book = {
+//     title: 'Human Acts',
+//     author: 'Han Kang'
+// }
 
-const bookJSON = JSON.stringify(book)
-console.log(bookJSON.title)
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json', bookJSON)
+
+
+// console.log(bookJSON)
+
+// const parsedData = JSON.parse(bookJSON)
+// console.log(parsedData.author)
+
+
+//What comes back IS NOT A STRING
+// it's a buffer which allows node.js
+// to represent binary data
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+// console.log(data.title)
+
+const dataBuffer = fs.readFileSync('1-json.json')
+dataJSON = dataBuffer.toString()
+const user = JSON.parse(dataJSON)
+
+user.name = 'Julia'
+user.age = 23
+
+const userJSON = JSON.stringify(user)
+fs.writeFileSync('1-json.json', userJSON)
---
+++ README.md
@@ -0,0 +1,5 @@
+# Node.js Notes
+
+# Learning goals
+* Review JSON and re-familiarize how it works with Javascript object and arrays
+* Store my Node.js notes in one place to make reviewing easier
