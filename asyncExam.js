60fa4330ae4c636339ac84df1bc093003dd0a9d0
Julia Fong <Juliamzfong@gmail.com>
2020-09-28 14:49:00 -0500

    Add async notes

---
+++ asyncExam.js
@@ -0,0 +1,13 @@
+console.log('Starting')
+
+setTimeout(() => {
+    console.log('2 second timers')
+}, 2000)
+
+
+setTimeout(() => {
+    console.log('right away timer')
+}, 0)
+
+
+console.log('Stopping')
